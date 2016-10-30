799deb85387c9f9b8335707a6ee38880db8ccfc0
unknown <hukaili0529@163.com>
2016-10-30 15:13:23 +0800

    init

---
+++ src/main/java/com/kaili/firstMavenPro/App.java
@@ -0,0 +1,13 @@
+package com.kaili.firstMavenPro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
